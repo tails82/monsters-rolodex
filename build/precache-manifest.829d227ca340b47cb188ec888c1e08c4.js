65a7f2eff4a3a340e332ea94c44dc8d2f8c43771
Zhang <wzhang@marinsoftware.com>
2019-08-14 17:19:46 +0800

    initial version

---
+++ build/precache-manifest.829d227ca340b47cb188ec888c1e08c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaec7bc2a27894dd65614dd0ff0cbd4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38e43f794934fa9bf2f8",
+    "url": "/monsters-rolodex/static/css/main.b820cf72.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "38e43f794934fa9bf2f8",
+    "url": "/monsters-rolodex/static/js/main.6398fde6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
